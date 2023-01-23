19639294edb695dcc1dd755634943205db3a8f0c
Coursera <aide.abdel@gmail.com>
2023-01-22 23:00:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+let name1 = "John";
+let name2 = "Marry";
+let name3= "James"
+let name4= "Jennifer"
+let name5= "Robert"
+let name6= "Linda"
+let name7= "Micheal"
+let name8="Barbara"
+let name9= "David"
+let name10= "Susan"
+
+let age1 = 10
+let age2 = 11
+let age3 = 12
+let age4 = 13
+let age5 = 14
+let age6 = 15
+let age7 = 16
+let age8 = 17
+let age9 = 18
+let age10 = 19
+
+let hobbies1 = ["Running","surfing","reading"];
+let hobbies2 = ["Drawin","surfing","reading"];
+let hobbies3 = ["Running","coding","reading"];
+let hobbies4 = ["Running","surfing","cooking"];
+let hobbies5 = ["napping","surfing","reading"];
+let hobbies6 = ["Running","basketball","reading"];
+let hobbies7 = ["soccer","surfing","reading"];
+let hobbies8 = ["Running","football","reading"];
+let hobbies9 = ["Running","surfing","fishing"];
+let hobbies10 = ["poetry","surfing","reading"];
+
+let person1 = {
+    firstName: name1,
+    age : age1,
+    hobbies: hobbies1,
+};
+
+let person2 = {
+    firstName: name2,
+    age : age2,
+    hobbies: hobbies2,
+};
+let person3 = {
+    firstName: name3,
+    age : age3,
+    hobbies: hobbies3,
+};
+let person4 = {
+    firstName: name4,
+    age : age4,
+    hobbies: hobbies4,
+};
+let person5 = {
+    firstName: name5,
+    age : age5,
+    hobbies: hobbies5,
+};
+let person6 = {
+    firstName: name6,
+    age : age6,
+    hobbies: hobbies6,
+};
+let person7 = {
+    firstName: name7,
+    age : age7,
+    hobbies: hobbies7,
+};
+let person8 = {
+    firstName: name8,
+    age : age8,
+    hobbies: hobbies8,
+};
+let person9 = {
+    firstName: name9,
+    age : age9,
+    hobbies: hobbies9,
+};
+let person10 = {
+    firstName: name10,
+    age : age10,
+    hobbies: hobbies10,
+};
+
+console.log(person10.hobbies)
